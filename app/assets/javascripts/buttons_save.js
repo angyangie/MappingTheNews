86aaf7ec29e4142fda406062bd53dfcb42cf911a
angyangie <angie.ramirez12@gmail.com>
2016-06-27 14:11:12 -0400

    removed country add button

--- app/assets/javascripts/buttons_save.js
+++ app/assets/javascripts/buttons_save.js
@@ -1,31 +1,6 @@
 $(document).ready(function() {
 
-  
-  $('.add_to_favorites_countries').on("click", function(e){
-    e.stopPropagation();
-    e.preventDefault();
-
-
-      var article_title = $(this.parentElement).clone().children().remove().end().text().trim()
-
-      var article_link = $(this).parent().find('a').attr('href')
-
-      // var data = { title: article_title, link: article_link }
-      var data = {}
-      data[article_title] = article_link
-
-      $.ajax({
-        type: "POST",
-        url: "/favorite_articles",
-        dataType: "JSON",
-        data: data
-        })
-  })
-
-//////
-
-
-  $('.add_to_favorites_articles').on("click", function(e){
+$('.add_to_favorites_articles').on("click", function(e){
 
     e.stopPropagation();
     e.preventDefault();
@@ -45,6 +20,30 @@ $(document).ready(function() {
         })
 
   })
+
+// $('.add_to_favorites_countries').on("click", function(e){
+//     e.stopPropagation();
+//     e.preventDefault();
+
+
+//       var article_title = $(this.parentElement).clone().children().remove().end().text().trim()
+
+//       var article_link = $(this).parent().find('a').attr('href')
+
+//       // var data = { title: article_title, link: article_link }
+//       var data = {}
+//       data[article_title] = article_link
+
+//       $.ajax({
+//         type: "POST",
+//         url: "/favorite_articles",
+//         dataType: "JSON",
+//         data: data
+//         })
+//   })
+
+//////
+
 
   // $('.save_for_later_countries').on("click", function(e){
 
